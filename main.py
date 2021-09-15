45ee0958aec03d4af177b6f15d94ccaa7690f171
ArmanZarei <armanzarei1378@gmail.com>
2021-09-15 11:05:26 +0430

    Grid view

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import tkinter as tk
 import argparse
 import os
 from utils import visualize
+import math
 
 
 def btn_clicked(btns, btn_idx, path_dir, file_name):
     btns[btn_idx].configure(foreground='green', activeforeground='green')
-    visualize(args.dir_path, f)
+    visualize(args.dir_path, file_name)
 
 
 if __name__=='__main__':
@@ -23,17 +24,17 @@ if __name__=='__main__':
             files.append(f.replace('.npy', ''))
 
     window = tk.Tk(className="Lettuce-Soil Annotated PointCloud Visualizer")
-    # window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}")
 
+    grid_size = math.ceil(math.sqrt(len(files)))
     btns = []
     for idx, f in enumerate(files):
         button = tk.Button(
             text=f,
             foreground='red',
             activeforeground='red',
-            command=lambda idx=idx: btn_clicked(btns, idx, args.dir_path, f)
+            command=lambda idx=idx, f=f: btn_clicked(btns, idx, args.dir_path, f),
         )
-        button.pack()
+        button.grid(row=idx//grid_size, column=idx%grid_size)
         btns.append(button)
 
     window.mainloop()
